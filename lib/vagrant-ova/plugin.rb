0c6d0e70094e542dbe471d4874acbdfd4f38e320
Marcos Peñate <mpenate@stratio.com>
2015-09-01 15:44:19 +0200

    modified:   lib/vagrant-ova/plugin.rb

--- lib/vagrant-ova/plugin.rb
+++ lib/vagrant-ova/plugin.rb
@@ -25,7 +25,7 @@ module VagrantPlugins
       def execute
         options = {}
         opts = OptionParser.new do |opts|
-          opts.banner = "Usage: vagrant ovf [vm-name]"
+          opts.banner = "Usage: vagrant ova [vm-name]"
         end
 
         argv = parse_options(opts) 
@@ -41,13 +41,11 @@ module VagrantPlugins
         mach.provider.driver.export(box_ovf)
         
 
-        puts "Translating OVF document..."
+        puts "Translating OVF document ..."
         doc = OVFDocument.parse(File.new(box_ovf), &:noblanks)
         doc.add_file(:href => 'Vagrantfile')
         doc.add_vmware_support
-        File.open(box_ovf, 'w') {|f| doc.write_xml_to f}
-
-        # rewrite SHA1 values of box.ovf & Vagrantfile
+        File.open(box_ovf, 'w') {|f| doc.write_xml_to f}        
         box_ovf_sha1 = OpenSSL::Digest::SHA1.hexdigest(File.read(box_ovf))
         vagrant_file_sha1 = OpenSSL::Digest::SHA1.hexdigest(File.read(vagrant_file))
         box_disk1_sha1 = OpenSSL::Digest::SHA1.hexdigest(File.read(box_disk1))
@@ -61,7 +59,7 @@ module VagrantPlugins
         doc = Document.new(file)
             
         stratio_module_version = doc.root.elements['version'].text
-        puts "Forming your ova file: "+ argv[0]+"-"+stratio_module_version+".ova"
+        puts "Forming your ova file: "+ argv[0]+"-"+stratio_module_version+".ova ..."
         files = [ argv[0]+'.ovf', argv[0]+'-disk1.vmdk', argv[0]+'.mf', 'Vagrantfile']      
           
         File.open(argv[0]+"-"+stratio_module_version+".ova", 'wb') do |f|
